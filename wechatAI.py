b69cb0162de158c9b85d13ecaa32fa73bfeb4dc5
YuanGao0702 <gaoyuan0702@gwu.edu>
2018-12-27 00:59:35 -0500

    add weather

--- wechatAI.py
+++ wechatAI.py
@@ -1,22 +1,22 @@
-import itchat  
+import itchat
 
 superlist = []
 
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
 	id = msg.fromUserName
-	if msg.text == "aaa": 
+	if msg.text == "aaa":
 		superlist.append(id)
-		itchat.send("Kingsman Mode!!!",toUserName=id) 
+		itchat.send("Kingsman Mode!!!",toUserName=id)
 		print("Kingsman Mode!!!")
 	if msg.text == "qqq":
 		superlist.remove(id)
-		itchat.send("Moron Mode!!!",toUserName=id) 
+		itchat.send("Moron Mode!!!",toUserName=id)
 		print("Moron Mode!!!")
 	if id in superlist:
 		if(msg.text=='weather'):
-			print("Weather is: Unknown")
-		#itchat.send(msg.text,toUserName=msg.fromUserName) 
+			itchat.send("Weather is: Unknown!!!",toUserName=id)
+		#itchat.send(msg.text,toUserName=msg.fromUserName)
 
-#itchat.auto_login()
-#itchat.run() 
+itchat.auto_login()
+itchat.run()
