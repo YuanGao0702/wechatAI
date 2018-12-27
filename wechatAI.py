d02547194bab5e001db6a76744a19f2edcc94282
yuangao0702 <gaoyuan0702@gwu.edu>
2018-12-26 22:28:59 -0500

    add mode

--- wechatAI.py
+++ wechatAI.py
@@ -1,5 +1,22 @@
-import itchat
+import itchat  
 
-itchat.auto_login()
+superlist = []
 
-itchat.send('Hello, filehelper', toUserName='Yuan')
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+	id = msg.fromUserName
+	if msg.text == "aaa": 
+		superlist.append(id)
+		itchat.send("Kingsman Mode!!!",toUserName=id) 
+		print("Kingsman Mode!!!")
+	if msg.text == "qqq":
+		superlist.remove(id)
+		itchat.send("Moron Mode!!!",toUserName=id) 
+		print("Moron Mode!!!")
+	if id in superlist:
+		if(msg.text=='weather'):
+			print("Weather is: Unknown")
+		#itchat.send(msg.text,toUserName=msg.fromUserName) 
+
+#itchat.auto_login()
+#itchat.run() 
